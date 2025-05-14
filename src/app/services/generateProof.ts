bf92606a2bf60469048cab9f5d61beda17f3aacc
Lucas Oliveira <olivmath@protonmail.com>
2025-05-14 18:04:12 -0300

    add: zk proof  v0.7

--- src/app/services/generateProof.ts
+++ src/app/services/generateProof.ts
@@ -1,27 +1,43 @@
 import { toast } from "sonner";
+
 export const generateProof = async (birthYear: number) => {
+  const { UltraPlonkBackend } = await import("@aztec/bb.js");
+  const { Noir } = await import("@noir-lang/noir_js");
   try {
-    // Initial setup
+    const res = await fetch("/circuit.json");
+    const circuit = await res.json();
+    const noir = new Noir(circuit);
+    const backend = new UltraPlonkBackend(circuit.bytecode);
 
-    toast.loading("Configurando sessão...");
-    // await 1 seconds
     toast.success("Sessão configurada");
-    toast.loading("Gerando witness...");
-    // await 1 seconds
-    toast.success("Witness gerado");
-    toast.loading("Gerando prova...");
-    // await 1 seconds
-    toast.success("Prova gerada");
 
-    fetch("/api/submit-proof", {
+    const { witness } = await noir.execute({
+      birth_year: birthYear,
+      current_year: 2025,
+    });
+    toast.success("Witness gerado");
+
+    const t1 = toast.loading("Gerando prova...");
+    const { proof, publicInputs } = await backend.generateProof(witness);
+    toast.success("Prova gerada");
+    toast.dismiss(t1);
+
+    const t2 = toast.loading("Gerando chave de verificação...");
+    const vk = await backend.getVerificationKey();
+    toast.dismiss(t2);
+    toast.success("Verificação de chave gerada");
+
+    const t3 = toast.loading("Submetendo provas para backend...");
+    await fetch("/api/submit-proof", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        proof: [1, 2, 3, 4, 5],
-        publicInputs: [1, 2, 3, 4, 5],
-        vk: [1, 2, 3, 4, 5],
+        proof,
+        publicInputs,
+        vk: vk,
       }),
     });
+    toast.dismiss(t3);
   } catch (err) {
     toast.error("Erro ao gerar prova");
     console.error("💔 Proof generation failed:", err);

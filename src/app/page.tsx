0ed944dbc0210716acaca66cb87fe3387af0d147
Lucas Oliveira <olivmath@protonmail.com>
2025-05-14 08:14:51 -0300

    fix: possition of connect wallet button works! v0.3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,34 @@
-'use client';
+"use client";
 
-import { TonConnectButton } from '@tonconnect/ui-react';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { TonConnectButton, useTonWallet } from "@tonconnect/ui-react";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+
+function HomeContent() {
+  const wallet = useTonWallet();
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-background to-foreground/5 relative">
+      <div className="text-center">
+        <h1 className="mb-4 font-mono text-4xl font-bold tracking-tight text-foreground/80 sm:text-6xl md:text-7xl">
+          Hello,
+          <br />{" "}
+          <span className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">
+            {wallet ? "World!" : "_ _ _ _ _ !"}
+          </span>
+        </h1>
+      </div>
+
+      <div className="fixed bottom-35 right-6">
+        <TonConnectButton className="tc-btn--up" />
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <TonConnectUIProvider manifestUrl="https://yellow-accused-earwig-831.mypinata.cloud/ipfs/bafkreif37arloqkzd76trz7edg534v26vqmd4ykxuwibgdelwr3m3xvbty">
-      <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-background to-foreground/5 relative">
-        <div className="text-center">
-          <h1 className="mb-4 font-mono text-4xl font-bold tracking-tight text-foreground/80 sm:text-6xl md:text-7xl">
-            Hello,{" "}
-            <span className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">
-              World!
-            </span>
-          </h1>
-        </div>
-        <div className="fixed bottom-6 right-6">
-          <TonConnectButton />
-        </div>
-      </div>
+      <HomeContent />
     </TonConnectUIProvider>
   );
 }

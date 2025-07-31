21e1341d9181b03f89d691b9607951963e8d8871
Lucas Oliveira <olivmath@protonmail.com>
2025-07-30 21:47:51 -0300

    fix

--- backendv2/src/server.js
+++ backendv2/src/server.js
@@ -3,6 +3,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando em http://0.0.0.0:${PORT}`);
 });
